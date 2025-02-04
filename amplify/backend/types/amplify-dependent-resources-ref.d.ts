4bf34949491873455a2aa43e6c5ca05b814447fe
meanzar <116259100+Meanzar@users.noreply.github.com>
2025-02-04 09:25:16 +0100

    reinit the project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "restoffapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
